dc60024c871c5ab35748cdf913e00663a0e9d260
Pournami Pradeep <pournamipradeep13@gmail.com>
2026-07-26 12:52:12 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,72 +1,23 @@
-import os
 import sys
-
-import subprocess
-
-def echo(statement):
-    print(statement.strip()[4:].strip())
-    return
-
-def check_exec(command):
-    paths = os.environ.get("PATH").split(":")
-    file_name = command
-    for dir in paths:
-        file_path = f"{dir}/{file_name}"
-        if os.path.exists(file_path):
-            if os.access(file_path, os.X_OK):
-                return (True, file_path)
-    return (False, None)
-
-def run_exec(statement):
-    splitted_statment = statement.split(" ")
-    command = splitted_statment[0]
-    executable, _ = check_exec(command)
-    if not executable:
-        print(f"{command}: command not found")
-        return
-    
-    result = subprocess.run(statement.split(" "),capture_output=True,text=True)
-    print(result.stdout,end='')
-
-
-def type_command(statement):
-    command = statement.strip()[4:].strip()
-    if command in ["type", "echo", "exit", "pwd"]:
-        print(f"{command} is a shell builtin")
-        return
-
-    executable, file_path = check_exec(command)
-    if executable:
-        print(f"{command} is {file_path}")
-        return
-    print(f"{command}: not found")
-    return
-
-def pwd(statement):
-    print(os.getcwd())
-    return
+from .utility import echo, type_command, pwd, run_exec, exit
         
-functions = {"echo":echo, "type": type_command, "pwd": pwd}
+functions = {"echo":echo, "type": type_command, "pwd": pwd, "exit": exit}
 def main():
     
     while True:
         sys.stdout.write("$ ")
         statement = input()
+        if not statement:
+            continue
         splitted_statment = statement.split(" ")
         command = splitted_statment[0]
-        if command == "exit":
-            break
-        if not command:
-            continue
+       
         if command in functions:
             functions[command](statement)
             continue
         
-        run_exec(statement)
+        run_exec(splitted_statment)
             
-    
-    
-
 
 if __name__ == "__main__":
     main()
---
+++ app/utility.py
@@ -0,0 +1,51 @@
+
+import os
+import sys
+
+import subprocess
+
+
+def exit(statement):
+    sys.exit()
+
+def echo(statement):
+    print(statement.strip()[4:].strip())
+    return
+
+def check_exec(command):
+    paths = os.environ.get("PATH").split(":")
+    file_name = command
+    for dir in paths:
+        file_path = f"{dir}/{file_name}"
+        if os.path.exists(file_path):
+            if os.access(file_path, os.X_OK):
+                return (True, file_path)
+    return (False, None)
+
+def run_exec(statement):
+    command = statement[0]
+    executable, _ = check_exec(command)
+    if not executable:
+        print(f"{command}: command not found")
+        return
+    
+    result = subprocess.run(statement,capture_output=True,text=True)
+    print(result.stdout,end='')
+
+
+def type_command(statement):
+    command = statement.strip()[4:].strip()
+    if command in ["type", "echo", "exit", "pwd"]:
+        print(f"{command} is a shell builtin")
+        return
+
+    executable, file_path = check_exec(command)
+    if executable:
+        print(f"{command} is {file_path}")
+        return
+    print(f"{command}: not found")
+    return
+
+def pwd(statement):
+    print(os.getcwd())
+    return
