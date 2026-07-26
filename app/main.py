55064e96ad5096f6a8e0010ef83f0145ca26d0c2
Pournami Pradeep <pournamipradeep13@gmail.com>
2026-07-26 08:30:24 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 import os
 import sys
 
-
+import subprocess
 
 def echo(statement):
     return statement.strip()[4:].strip()
+
+def check_exec(command):
+    paths = os.environ.get("PATH").split(":")
+    file_name = command
+    for dir in paths:
+        file_path = f"{dir}/{file_name}"
+        if os.path.exists(file_path):
+            if os.access(file_path, os.X_OK):
+                return True
+    return False
 
 
 def type_command(statement):
@@ -28,18 +38,31 @@ def main():
     while True:
         sys.stdout.write("$ ")
         statement = input()
-        command = statement.split(" ")[0]
+        splitted_statment = statement.split(" ")
+        command = splitted_statment[0]
         if not command:
             continue
         if command == "exit":
             break
-        elif command == "echo":
+        if command == "echo":
             print(echo(statement))
-        elif command == "type":
+            continue
+        
+        if command == "type":
             result = type_command(statement)
             if not result:
                 continue
             print(result)
+            continue
+        
+        exec = check_exec(command)
+        if exec:
+            subprocess.run(" ".join(splitted_statment[1:]))
+        
+
+           
+            
+            
         else:
             print(f"{command}: command not found")
     
