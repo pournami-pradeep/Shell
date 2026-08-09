c7d7cb54e7f7783cc53c7676e1ca01041f200bf7
Pournami Pradeep <pournamipradeep13@gmail.com>
2026-08-09 10:16:15 +0530

    codecrafters submit [skip ci]

--- app/utility.py
+++ app/utility.py
@@ -74,7 +74,7 @@ def cd(statement):
         elif path == "~":
             new_path = HOME
         else:
-            new_path += f"/{path}"
+            new_path += f"{path}"
             
         if os.path.exists(new_path):
             os.chdir(new_path)
